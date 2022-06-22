2e6d690373b938e3a1efa35de1f39ba1ce1bad90
Duncan Scott <duncan_scott@outlook.com>
2022-06-22 08:55:47 +0100

    Fixed nav link spacing, made collapse only happen for much smaller widths

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -3,15 +3,26 @@ import { NavLink } from "react-router-dom";
 
 export const NavigationBar = () => {
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="light" expand="sm">
             <Container>
                 <Navbar.Brand href="#home">Duncan Scott</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav"> 
                     <Nav className="ms-auto">
-                        <NavLink to="/">Home</NavLink>
-                        <NavLink to="/about">About</NavLink>
-                        <NavLink to="/contact">Contact</NavLink>
+                        {/*
+                            This is quite confusing. NavLink comes from react-router-dom and handles the linking. However,
+                            there's no styling for it. Nav.Link from the React Bootstrap components provides the actual
+                            styling
+                        */}
+                        <Nav.Link>
+                            <NavLink to="/">Home</NavLink>
+                        </Nav.Link>
+                        <Nav.Link>
+                            <NavLink to="/about">About</NavLink>
+                        </Nav.Link>
+                        <Nav.Link>
+                            <NavLink to="/contact">Contact</NavLink>
+                        </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
