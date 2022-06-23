2380c672173079294bcbbe48e2733a43c964a53d
Duncan Scott <duncan_scott@outlook.com>
2022-06-23 09:18:26 +0100

    Removed CSS, added SASS in preparation for colour customisation

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,3 @@
-import "./App.css";
-
 export const Home = () => {
     return (
         <div className="App">
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOMClient from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { HashRouter } from "react-router-dom";
+import "./scss/custom.scss";
 
 const container = document.getElementById("root") as HTMLElement;
 const root = ReactDOMClient.createRoot(container);
