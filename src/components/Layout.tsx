21912f4c924c91b0e8df13576d56660a3e2f9280
Duncan Scott <duncan_scott@outlook.com>
2022-10-03 22:41:42 +0100

    Added Contact page, add margin below navbar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import { Col, Container, Row } from "react-bootstrap";
 
 export const Layout = ({ children }: PropsWithChildren<Record<never, never>>) => {
     return (
-        <Container className="flex-fill" fluid>
+        <Container className="flex-fill mt-2" fluid>
             <Row>
                 <Col />
                 <Col xs={10} md={8} lg={6}>
--- src/views/Contact.tsx
+++ src/views/Contact.tsx
@@ -3,9 +3,9 @@ import { Layout } from "../components/Layout";
 export const Contact = () => {
     return (
         <Layout>
-            <h2>Contact Page</h2>
+            <h2>Contact</h2>
             <main>
-                <p>This is the Contact page</p>
+                <p>Please use <a href="https://www.linkedin.com/in/duncan-scott-ba10324/" target="_blank" rel="noopener noreferrer">LinkedIn</a> to contact me.</p>
             </main>
         </Layout>
     );
